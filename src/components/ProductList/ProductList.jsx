56ddc339fd8d79ed3c28363fa7bb697bc22b2774
glintwine <a.alekseev@rigintelpro.ru>
2024-12-13 03:44:06 +0500

    s

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,71 +1,71 @@
-import React, {useState} from 'react';
-import './ProductList.css';
+import React, {useCallback, useEffect, useState} from 'react';
+import './ProductList.css'
 import ProductItem from "../ProductItem/ProductItem";
-import {useTelegram} from "../../hooks/useTelegram";
-import {useCallback, useEffect} from "react";
+import {useTelegram} from "../Hooks/UseTelegram";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Видеокарта', price: 45000, description: 'RTX 4060'},
+    {id: '2', title: 'Процессор', price: 25000, description: 'R7 7800x3d'},
+    {id: '3', title: 'Блок питания', price: 45000, description: 'beQuite 850w'}
 ]
 
-const getTotalPrice = (items = []) => {
+const getTotalPrice = (items) => {
     return items.reduce((acc, item) => {
-        return acc += item.price
-    }, 0)
+        return acc += item.price;
+    }, 0);
 }
 
 const ProductList = () => {
-    const [addedItems, setAddedItems] = useState([]);
-    const {tg, queryId} = useTelegram();
 
-    const onSendData = useCallback(() => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg, queryId} = useTelegram()
+
+    const onSendData = useCallback(() =>{
         const data = {
             products: addedItems,
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('htttp://91.105.196.176:4000/web-data',{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(data)
-        })
-    }, [addedItems])
+        });
+    }, [addedItems]);
 
-    useEffect(() => {
+
+    useEffect(()=>{
         tg.onEvent('mainButtonClicked', onSendData)
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
     }, [onSendData])
 
+
+
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
 
-        if(alreadyAdded) {
+        if (alreadyAdded) {
             newItems = addedItems.filter(item => item.id !== product.id);
-        } else {
+        }
+        else{
             newItems = [...addedItems, product];
         }
 
-        setAddedItems(newItems)
+        setAddedItems(newItems);
 
-        if(newItems.length === 0) {
+        if(newItems.length === 0){
             tg.MainButton.hide();
-        } else {
+        }
+        else{
             tg.MainButton.show();
             tg.MainButton.setParams({
                 text: `Купить ${getTotalPrice(newItems)}`
-            })
+            });
         }
     }
 
